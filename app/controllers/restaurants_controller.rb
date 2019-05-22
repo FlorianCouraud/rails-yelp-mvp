6636a461adfad95d92359c70b9699054c88125d2
Florian Couraud <f.couraud13@gmail.com>
2019-05-22 17:10:42 +0200

    restaurantsbis

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,17 @@
+class RestaurantsController < ApplicationController
+  def index
+    fff
+  end
+
+  def new
+    qqqqq
+  end
+
+  def create
+    hhhhhh
+  end
+
+  def show
+    mlkjhgfd
+  end
+end
